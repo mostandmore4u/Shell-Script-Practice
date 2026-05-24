896ec5cfd1454180556fdca4a65e9d02fbd026fc
mostandmore4u <mostandmore4u@gmail.com>
2026-05-24 23:16:29 +0530

    special variables

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 NUM1=10
-NUM2=linux
+NUM2=33
+#linux
 
 SUM=$(($NUM1+$NUM2))
 
 echo "Sum is: $SUM"
+
+# Array
+MOVIES=("RRR" "Varanasi" "Pushpa") # index always starts from 0
+echo "Movies are: ${MOVIES[@]}"
