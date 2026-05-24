7b8b9e64668a88dbcf216afd54b032a25568cba1
mostandmore4u <mostandmore4u@gmail.com>
2026-05-24 18:34:31 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "Trump: Hey Iran, we are going to blast you"
+echo "Iran: Yes Please , we are waiting"
+echo "Trump: Hey Iran, Im very much serious about it"
+echo "Iran: We are also serious"
+
+echo "$Person1: Hey $Person2, we are going to blast you"
+echo "$Person2: Yes Please , we are waiting"
+echo "$Person1: Hey $Person2 Im very much serious about it"
+echo "$Person2: We are also serious"
