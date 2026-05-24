98403aa4c95df1744ecb43f2eaad82109691d54a
mostandmore4u <mostandmore4u@gmail.com>
2026-05-24 19:15:42 +0530

    variables silent

--- 04-variables silent.sh
+++ 04-variables silent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo  "Please Enter your UserName:: "
+echo  "Please Enter your username:: "
 read USER_NAME # here USER_NAME is a variable , what ever you enter in the terminal stores in this variable
-echo "Username is USER_NAME"
+echo "Username is $USER_NAME"
