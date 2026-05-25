b004e1ad1d394a940c4e945f6aa4abbc570a7ec3
mostandmore4u <mostandmore4u@gmail.com>
2026-05-25 17:44:25 +0530

    loop

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log" 
-TIMESTAMP=$(date "=%Y-%m-%d %H-%M-%S")
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 # Check root access or not
 if [ $USERID -ne 0 ]; then
